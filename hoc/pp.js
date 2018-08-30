2bd703d6d1e32f448da31337a8a23c4a59054d4d
liumin1128 <970568830@qq.com>
2018-08-30 22:06:09 +0800

    更新

--- hoc/pp.js
+++ hoc/pp.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 
-export default function (WrappedComponent, props) {
+export default function pp(WrappedComponent, props) {
   return class extends PureComponent {
     render() {
       return <WrappedComponent {...this.props} {...props} />;
