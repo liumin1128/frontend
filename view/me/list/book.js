cfb0a77c4ff4486b730fe326e4211ef9f2b4abaa
liumin1128 <970568830@qq.com>
2018-09-03 21:27:00 +0800

    更新

--- view/me/list/book.js
+++ view/me/list/book.js
@@ -88,7 +88,7 @@ export default class BookList extends PureComponent {
                               <ImageIcon />
                             </Avatar>
                             <ListItemText
-                              primary={`i booked the meeting：${i.timetable.title}`}
+                              primary={`i booked the meeting：${i.timetable ? i.timetable.title : '活动已删除'}`}
                               secondary={moment(i.createdAt).format('llll')}
                             />
                           </ListItem>
